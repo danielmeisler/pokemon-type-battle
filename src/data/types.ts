006c9ea505504a60529e16c2767fe97c877962db
Daniel Meisler <daniel_meisler@web.de>
2025-06-20 18:32:06 +0200

    feat: add all types wip

--- src/data/types.ts
+++ src/data/types.ts
@@ -2,22 +2,22 @@ import { msg } from '@lit/localize';
 
 type Type =
 	| 'normal'
+	| 'fighting'
+	| 'flying'
+	| 'poison'
+	| 'ground'
+	| 'rock'
+	| 'bug'
+	| 'ghost'
+	| 'steel'
 	| 'fire'
 	| 'water'
 	| 'grass'
 	| 'electric'
+	| 'psychic'
 	| 'ice'
-	| 'fighting'
-	| 'poison'
-	| 'ground'
-	| 'flying'
-	| 'psychich'
-	| 'bug'
-	| 'rock'
-	| 'ghost'
 	| 'dragon'
 	| 'dark'
-	| 'steel'
 	| 'fairy';
 
 interface PokemonType {
@@ -25,7 +25,8 @@ interface PokemonType {
 	color: string;
 	icon: string;
 	effective: Type[];
-	weakness: Type[];
+	ineffective: Type[];
+	nullified: Type[];
 }
 
 type Types = Record<Type, PokemonType>;
@@ -33,12 +34,148 @@ type Types = Record<Type, PokemonType>;
 const types: Types = {
 	normal: {
 		label: msg('Normal'),
-		color: '#A8A77A',
+		color: '#A0A2A0',
 		icon: '/assets/types/normal.svg',
 		effective: [],
-		weakness: [],
+		ineffective: ['rock', 'steel'],
+		nullified: ['ghost'],
 	},
-	// Gedanken machen zu Attack/Defend Effective/Weakness?...
+	fighting: {
+		label: msg('Fighting'),
+		color: '#FF8100',
+		icon: '/assets/types/fighting.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	flying: {
+		label: msg('Flying'),
+		color: '#82BAEF',
+		icon: '/assets/types/flying.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	poison: {
+		label: msg('Poison'),
+		color: '#923FCC',
+		icon: '/assets/types/poison.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	ground: {
+		label: msg('Ground'),
+		color: '#92501B',
+		icon: '/assets/types/ground.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	rock: {
+		label: msg('Rock'),
+		color: '#B0AB82',
+		icon: '/assets/types/rock.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	bug: {
+		label: msg('Bug'),
+		color: '#92A212',
+		icon: '/assets/types/bug.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	ghost: {
+		label: msg('Ghost'),
+		color: '#713F71',
+		icon: '/assets/types/ghost.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	steel: {
+		label: msg('Steel'),
+		color: '#60A2B9',
+		icon: '/assets/types/steel.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	fire: {
+		label: msg('Fire'),
+		color: '#E72324',
+		icon: '/assets/types/fire.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	water: {
+		label: msg('Water'),
+		color: '#2481EF',
+		icon: '/assets/types/water.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	grass: {
+		label: msg('Grass'),
+		color: '#3CA124',
+		icon: '/assets/types/grass.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	electric: {
+		label: msg('Electric'),
+		color: '#FAC100',
+		icon: '/assets/types/electric.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	psychic: {
+		label: msg('Psychic'),
+		color: '#EF3F7A',
+		icon: '/assets/types/psychic.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	ice: {
+		label: msg('Ice'),
+		color: '#3DD9FF',
+		icon: '/assets/types/ice.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	dragon: {
+		label: msg('Dragon'),
+		color: '#4E5FE2',
+		icon: '/assets/types/dragon.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	dark: {
+		label: msg('Dark'),
+		color: '#4E3F3C',
+		icon: '/assets/types/dark.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
+	fairy: {
+		label: msg('Fairy'),
+		color: '#EF71EF',
+		icon: '/assets/types/fairy.svg',
+		effective: [],
+		ineffective: [],
+		nullified: [],
+	},
 };
 
 export { types, type Type };
